17c0b1c298a1bf79443c9fe9acfad47381e09121
jinminxi <jinminxi@pjlab.org.cn>
2023-04-03 21:27:00 +0800

    register ascendgraph backend

---
+++ torch/_dynamo/backends/ascendgraph.py
@@ -0,0 +1,8 @@
+from torch._dynamo import register_backend
+
+
+@register_backend
+def ascendgraph(gm, fake_input_tensor):
+    from third_party.DICP.common.compile_fx import compile_fx
+
+    return compile_fx(gm, fake_input_tensor, "ascendgraph")
