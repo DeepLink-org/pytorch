10dbbd6c4d5363bf4fb5135cdca67a5275cf8380
chenchiyu <chenchiyu@pjlab.org.cn>
2023-04-28 14:08:14 +0800

    remove DICP submodule: we can remove the dependency of DICP in pytorch repo due to the usage of entry_points in torch_dynamo_backends

--- torch/_dynamo/backends/topsgraph.py
+++
@@ -1,8 +0,0 @@
-from torch._dynamo import register_backend
-
-
-@register_backend
-def topsgraph(gm, fake_input_tensor):
-    from third_party.DICP.common.compile_fx import compile_fx
-
-    return compile_fx(gm, fake_input_tensor, "topsgraph")
